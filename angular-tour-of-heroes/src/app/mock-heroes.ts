499a90f18a2042e7505c8ad70a01e1d5181fb517
jmorales113 <jmorales113@gmail.com>
2021-01-31 14:37:11 -0800

    Rendered heroes list

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero"
+
+export const HEROES: Hero[] = [
+  { id: 11, name: "Squall" },
+  { id: 12, name: "Luneth" },
+  { id: 13, name: "Firion" },
+  { id: 14, name: "Vaan" },
+  { id: 15, name: "Lightning" },
+  { id: 16, name: "Terra" },
+  { id: 17, name: "Noctis" },
+  { id: 18, name: "Cecil" },
+  { id: 19, name: "Tidus" },
+  { id: 20, name: "Zidane" }
+]
