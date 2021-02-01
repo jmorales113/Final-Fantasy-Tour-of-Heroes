af63eb2964e98748637b2547153e3bc17d157d4b
jmorales113 <jmorales113@gmail.com>
2021-01-31 16:00:30 -0800

    Rendered list of heroes with click event

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -5,19 +5,23 @@ import { HEROES} from "../mock-heroes"
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
-    name: "Cloud"
+    name: 'Cloud',
+  };
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
-  heroes = HEROES
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
