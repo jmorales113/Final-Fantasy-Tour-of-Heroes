f3e6e112a867221b75e1faf69eb10044fb1cfd81
jmorales113 <jmorales113@gmail.com>
2021-02-01 18:57:20 -0800

    Added services and styling

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { Hero } from "../hero"
-import { HEROES} from "../mock-heroes"
+import { MessageService} from "../message.service"
+import { HeroService } from "../hero.service"
 
 @Component({
   selector: 'app-heroes',
@@ -8,20 +9,24 @@ import { HEROES} from "../mock-heroes"
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Cloud',
-  };
 
-  heroes = HEROES;
+  selectedHero: Hero
 
-  selectedHero: Hero;
+  heroes: Hero[]
 
-  constructor() {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.getHeroes()
+  }
 
   onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+    this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
-}
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes)
+  }
+ }
