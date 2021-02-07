bf1f49d5a5b5646526141c23216a8708398bec2a
jmorales113 <jmorales113@gmail.com>
2021-02-06 16:12:30 -0800

    Added more routes and styling

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core'
 import { Hero } from "../hero"
-import { MessageService} from "../message.service"
 import { HeroService } from "../hero.service"
 
 @Component({
@@ -9,24 +8,16 @@ import { HeroService } from "../hero.service"
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero
-
   heroes: Hero[]
 
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes()
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes)
+    .subscribe(heroes => this.heroes = heroes)
   }
- }
+}
